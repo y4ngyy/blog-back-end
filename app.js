371978fb0ba3123d112894bfb49031ce90acde46
y4ngyy <591620892@qq.com>
2019-07-26 15:14:25 +0800

    remove package

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var api = require('./routers/api');
-var index = require('./routers/index');
 
 var app = express();
 // 添加json解析
@@ -11,7 +10,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
 app.use('/api', api);
-app.use('/', index);
 
 app.listen(5000, function() {
 	console.log('Server listening on port 5000');
